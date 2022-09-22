88d88e21f2b7eb8d8155456d656cfbda92b961d0
DRAVASP SHROFF <27689043+dravasp@users.noreply.github.com>
2022-09-22 12:34:12 +0530

    Update registration.php
    Added missing php end tag
    
    ?>
    
    Signed-off-by: DRAVASP SHROFF <27689043+dravasp@users.noreply.github.com>

--- registration.php
+++ registration.php
@@ -1,7 +1,13 @@
 <?php
 /**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+/**
  * Cashfree payments module for Magento 2
  */
 
 use \Magento\Framework\Component\ComponentRegistrar;
 ComponentRegistrar::register(ComponentRegistrar::MODULE,'Cashfree_Cfcheckout',__DIR__);
+?>
